c92689f89372c297aae19ab749580507293735eb
ivongarz2012 <ivongarz2012@gmail.com>
2026-06-19 18:28:19 -0500

    Update MiComponente.js

--- frontend/src/components/test.txt/MiComponente.js
+++ frontend/src/components/test.txt/MiComponente.js
@@ -1,4 +1,8 @@
+/*
+ * Cambio documentado por Ivón para validación automática en CircleCI
+ */
 function MiComponente() {
+
     return (
         <div>
             <h1>¡Hola, este es mi primer componente!</h1>
